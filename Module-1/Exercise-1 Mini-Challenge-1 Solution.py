3aababb3216ef35b9d9958bcfce07ea32e6574ee
Royce Chua <royce236@gmail.com>
2020-04-26 01:25:39 +0800

    Add Exercise 1 Solution

---
+++ Module-1/Exercise-1 Mini-Challenge-1 Solution.py
@@ -0,0 +1,12 @@
+# This is a temperature converter program
+
+# Pre-assign the celsius value as mentioned in the instruction
+celsius = 27.3
+# Compute the value of the Fahrenheight equivalent and store it in fahrenheit
+fahrenheit = (celsius * (9/5)) + 32
+# Comptue the value of the Kelvin equivalent
+kelvin = celsius + 273.15
+# Display the values
+print(f"Celsius value is: {celsius}")
+print(f"Fahrenheit value is: {fahrenheit}")
+print(f"Kelvin value is: {kelvin}")
